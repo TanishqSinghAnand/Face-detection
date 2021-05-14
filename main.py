a579bb7c0bd44923b906f0c1bf0f110c6d170bbb
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-05-15 00:15:19 +0530

    Done with image face detection

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import cv2
-
+import random
 trained_face_data = cv2.CascadeClassifier(
     "haarcascade_frontalface_default.xml")
 
-img = cv2.imread('ch.jpg')
+img = cv2.imread('ch.png')
 
-gray_scaled_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
+print(face_coordinates)
 
-face_coordinates = 
+for (x, y, w, h) in face_coordinates:
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rw = random.randint(1,10)
+    cv2.rectangle(img, (x, y), (x+w, y+h),
+                  (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)), random.randint(3,6))
 
-cv2.imshow('THOR', gray_scaled_img)
+cv2.imshow('THOR', img)
 cv2.waitKey()
 
 
 print("Code compiled succefully")
-
-
