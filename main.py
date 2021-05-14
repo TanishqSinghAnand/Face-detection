fdc1b1c2f5fb61d58d720d0bfcc3bf2d68552431
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-05-15 00:52:06 +0530

    Added real time video face detection

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import random
 trained_face_data = cv2.CascadeClassifier(
     "haarcascade_frontalface_default.xml")
 
-img = cv2.imread('ch.png')
+#img = cv2.imread('class.png')
+webcam = cv2.VideoCapture(0)
 
-gray_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
-print(face_coordinates)
-
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+w, y+h),(random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)), random.randint(3, 6))
-
-cv2.imshow('THOR', img)
-cv2.waitKey()
-
+while True:
+    frame_read_t_or_f, frame = webcam.read()
+    gray_scaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (random.randint(0, 256),
+                                                  random.randint(0, 256), random.randint(0, 256)), random.randint(3, 6))
+    cv2.imshow('lmao', frame)
+    cv2.waitKey(1)
 
 print("Code compiled succefully")
